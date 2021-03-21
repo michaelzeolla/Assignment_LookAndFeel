d692ed69f461cc1ea43b0548cac67f2e4d7b41fe
samratkorupolu1 <samratkorupolu1@gmail.com>
2021-03-21 12:41:47 -0400

    Deleted all comments

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
-/**
- * Your JS code here
- */
 
-/**
- * Sample code
- */
 document.addEventListener('DOMContentLoaded', function() {
   var ulElement = document.getElementById('links');
   var liElement = document.createElement('li');
